2c44aece2e830d697de8ef705d77b1c3906443d6
ADITYA PRASETYO <adtzslowy@mac.local>
2026-03-11 23:29:59 +0700

    updated

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:sync-iot')->everyFiveSeconds();
+Schedule::command('app:sync-iot')->everyTwoSeconds();
