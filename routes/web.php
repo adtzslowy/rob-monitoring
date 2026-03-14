6218f1896636154e7d51a16e9d264f5308f27be1
adtzslowy <aditzkun0987@gmail.com>
2026-03-15 05:42:44 +0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Livewire\Admin\PetaMonitoring;
 use App\Livewire\Admin\Profile;
 use App\Livewire\Admin\SensorList;
 use App\Livewire\Admin\UserManagement;
+use App\Services\TelegramServices;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,6 +22,13 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+
+Route::post('/telegram/webhook', function (Request $request) {
+   $update = $request->json()->all() ;
+   app(TelegramServices::class)->handleCommand($update);
+   return response()->json(['ok' => true]);
+})->withoutMiddleware([]);
+
 
 /**
  * Dashboard routing
